1c9742cb18437c6232413557ce7ce79026aa59b0
suhaibmuhd01 <suhaibmuhd04@gmail.com>
2025-01-18 14:50:32 +0100

    Implemented Sass for improved CSS organization and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        customPurple: '#6a1b9a',
         primary: "#1D4ED8",
         secondary: "#9333EA",
         accent: "#e6dfec",
+        customBlue: '#3b82f6',
+        customPink: '#ec4899',
+        customYellow: '#facc15',
       },
       fontFamily: {
         sans: ["Roboto", "sans-serif"],
